2f9faea8e10e09bdac4424cfad87495fac59042d
RoKols2017 <RoKols2017@gmail.com>
2025-05-14 22:47:33 +0300

    Before adding User application

--- event_booking/urls.py
+++ event_booking/urls.py
@@ -10,3 +10,5 @@ urlpatterns = [
     path('', include('events.urls')),
     path('accounts/', include('django.contrib.auth.urls')),  # login/logout
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
